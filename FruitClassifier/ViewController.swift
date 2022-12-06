232a7d68d5293c29edb971522df1d23b0648b624
Shandor Balogh <shandor.balogh@coaxsoft.com>
2022-12-06 22:09:25 +0100

    Initial Commit

---
+++ FruitClassifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FruitClassifier
+//
+//  Created by coax on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
